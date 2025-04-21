7a70f314f7a6ac44316e8a507ca55e507056f889
muna <munapm1@gmail.com>
2025-04-21 10:31:49 +0400

    added form to homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  standalone: true,
+  imports: [CommonModule, FormsModule,HeaderComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  keyword = '';
+  selectedType = '';
+  selectedLocation = '';
+  locationText = '';
 
+  onSearch() {
+    console.log('Searching with:', {
+      keyword: this.keyword,
+      type: this.selectedType,
+      location: this.selectedLocation,
+      locationText: this.locationText
+    });
+
+    // You can implement your actual search logic here
+  }
 }
