90d4b85a7ca45310c29abbc0cedaa20ed9913b0a
muna <munapm1@gmail.com>
2025-04-23 14:53:14 +0400

    Implement responsive homepage design

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../shared/header/header.component';
+import { Component, ViewChild, TemplateRef } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -25,5 +27,28 @@ export class HomeComponent {
     });
 
     // You can implement your actual search logic here
+  }
+
+  //##### section4
+
+  @ViewChild('videoModal') videoModalRef!: TemplateRef<any>;
+  showModal = false;
+  sanitizedVideoUrl!: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  openVideoModal(): void {
+    // const videoId = 'dQw4w9WgXcQ'; 
+    // const videoUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+    const videoUrl = 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1';
+    this.sanitizedVideoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(videoUrl);
+    this.showModal = true;
+    document.body.classList.add('modal-open');
+  }
+
+  closeVideoModal(): void {
+    this.sanitizedVideoUrl = '';
+    this.showModal = false;
+    document.body.classList.remove('modal-open');
   }
 }
